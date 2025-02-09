8219c5d2ec85f86c867e991e4bc15b2230385c41
Nikolai Escondo <nescondo2@gmail.com>
2025-02-08 19:40:50 -0500

    functionality for conversion added

--- video_converter.sh
+++ video_converter.sh
@@ -1,15 +1,31 @@
 #! /usr/bin/bash
 
-# simple script to download a youtube video and convert it to a .mp4
+# simple script to convert a video file to an .flv
 
-download() {
-	link=$1
-	echo "Enter YouTube link: $link"
-	wget $link
-	mv ~/watch?* ~/yt/downloads
+#store () {
+#	file=$1
+#	echo "Enter video file: $file"
+#	mv ~/$file ~/video-converter/downloads
+#}
+
+convert () {
+	vid_type=(.flv .mp4 .avi .mov)
+	if [[ $u_type == ${vid_type[0]} ]]; then
+		ffmpeg -i $video $video.flv
+	elif [[ $u_type == ${vid_type[1]} ]]; then
+		ffmpeg -i $video $video.mp4
+	elif [[ $u_type == ${vid_type[2]} ]]; then
+		ffmpeg -i $video $video.avi
+	elif [[ $u_type == ${vid_type[3]} ]]; then
+		ffmpeg -i $video $video.mov
+	fi
+}
+		
+	
+play () {
+	vlc = /mnt/c/Users/nesco/C:/Program Files/VideoLAN/VLC/vlc.exe
 }
 
-convert() {
-	
-
-download $1
+u_type=$1
+video=$2
+convert $u_type
